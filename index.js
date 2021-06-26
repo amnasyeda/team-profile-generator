24823fd9f7f1300356132fd6fbe92cbafeed92c0
Amna Syeda <amna.syeda98@gmail.com>
2021-06-25 21:37:16 -0400

    initial code for index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const inquirer = require('inquirer');
+const fs = require("fs");
+const Manager = require("./lib/manager.js");
+const Employee = require("./lib/employee.js");
+const Engineer = require("./lib/engineer.js");
+const Intern = require("./lib/intern.js");
+
+const teamMembers = [];
+
+startApp ();
+addAnother ();
+generateHTML();
