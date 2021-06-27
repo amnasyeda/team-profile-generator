74f9b8b8e7169c191be872ee43b997627236507c
Amna Syeda <amna.syeda98@gmail.com>
2021-06-27 12:13:43 -0400

    index.js call functions

--- index.js
+++ index.js
@@ -1,136 +1,220 @@
 const inquirer = require('inquirer');
 const fs = require("fs");
 const Manager = require("./lib/manager.js");
-const Employee = require("./lib/employee.js");
 const Engineer = require("./lib/engineer.js");
 const Intern = require("./lib/intern.js");
 const generateMarkdown = require('./src/generateMarkdown.js');
+const renderHTML = require("./lib/generateHTML");
 const allEmployees = [];
+
+let employeesArr = [];
 
 const questions = [
     {
-        type: 'list',
-        name: 'role',
-        message: 'What is the position/role of the employee?',
-        choices: ['Manager', 'Employee','Engineer', 'Intern']
-    },
-    {
-        type: 'input',
-        name: 'firstName',
-        message: ({ role }) => `What is the indivisual's first name?`,
-        validate: nameInput => {
-            if (nameInput) {
+        type: "input",
+        name: "name",
+        message: "What is the name of the employee? (Required)",
+        validate: name => {
+            if (name) {
                 return true;
             } else {
-                console.log('Please enter a name!');
+                console.log("Please enter the name of this employee!");
                 return false;
             }
         }
     },
     {
-        type: 'input',
-        name: 'lastName',
-        message: ({ firstName }) => `What is the indivisual's last name?`,
-        validate: nameInput => {
-            if (nameInput) {
+        type: "input",
+        name: "idNumber",
+        message: "What is the ID number of the employee? (Required)",
+        validate: StaffID => {
+            if (StaffID) {
                 return true;
             } else {
-                console.log('Please enter a name!');
+                console.log("Please enter the employee's ID number!");
                 return false;
             }
         }
     },
     {
-        type: 'input',
-        name: 'id',
-        message: ({ firstName }) => `What is the indivisual's ID number?`,
-        validate: idInput => {
-            if (!isNaN(parseInt(idInput))) {
+        type: "input",
+        name: "email",
+        message: "What is the employee's email address? (Required)",
+        validate: email => {
+            if (email) {
                 return true;
             } else {
-                console.log('Please enter a valid ID number!');
-                return false;
-            }
-        }
-    },
-
-    {
-        type: 'input',
-        name: 'github',
-        message: ({ firstName }) => `What is the indivisual's GitHub userame?`,
-        when: ({ role }) => {
-            if (role === 'engineer') {
-                return true;
-            } else {
-                return false;
-            }
-        },
-        validate: githubInput => {
-            if (githubInput) {
-                return true;
-            } else {
-                console.log('Please enter a username!');
+                console.log("Please enter the employees's email address!");
                 return false;
             }
         }
     },
     {
-        type: 'input',
-        name: 'school',
-        message: ({ firstName }) => `What school does the indivisual's go to?`,
-        when: ({ role }) => {
-            if (role === 'Intern') {
+        type: "list",
+        name: "role",
+        message: "What is the role of the employee? (Required)",
+        choices: ["Engineer", "Intern", "Manager"],
+        validate: staffRole => {
+            if (staffRole) {
                 return true;
             } else {
-                return false;
-            }
-        },
-        validate: schoolInput => {
-            if (schoolInput) {
-                return true;
-            } else {
-                console.log('Please enter a valid school name!');
+                console.log("Please select the employee's role!");
                 return false;
             }
         }
-    },
-    {
-        type: 'confirm',
-        name: 'addAdditional',
-        message: 'Would you like to add an additional employee?',
-        default: true
     }
-]
+    ]
 
-const promptStaff = () => {
+    // Manager question
+    managerQuestions = [
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "What is the manager's office number? (Required)",
+            validate: officeNumber => {
+                if (officeNumber) {
+                  return true;
+                } else {
+                  console.log("Please enter an office number!");
+                  return false;
+                },
+              },
+              {
+                type: "list",
+                name: "addNew",
+                message: "Do you want to add another employee",
+                choices: ["yes", "no"]
+            }
+         ],
 
-    return inquirer.prompt(questions)
-    .then(staffResponse => {
+    // Engineer Question
+    engineerQuestions = [
+        {
+            type: "input",
+            name: "github",
+            message: "What is the engineer's GitHub Username? (Required)",
+            validate: github => {
+                if (github) {
+                  return true;
+                } else {
+                  console.log("Please enter a GitHub username!");
+                  return false;
+                },
+            },
+            {
+              type: "list",
+              name: "addNew",
+              message: "Do you want to add another employee",
+              choices: ["yes", "no"]
+          }
+       ],
 
-        allEmployees.push(staffResponse);
+    // Intern Question
+    internQuestions = [
+        {
+            type: "input",
+            name: "school",
+            message: "What school did the intern attend? (Required)",
+            validate: school => {
+                if (school) {
+                  return true;
+                } else {
+                  console.log("Please enter a valid school name!");
+                  return false;
+                },
+            },
+            {
+              type: "list",
+              name: "addNew",
+              message: "Do you want to add another employee",
+              choices: ["yes", "no"]
+          }
+       ]
 
-        if (staffResponse.addAdditional) {
-            return promptStaff();
-        } else {
-            return allEmployees;
-        };
-    });
-};
+    // Employee's role
+    const seletRole = [
+        {
+            type: "list",
+            name: "selectRole",
+            message: "Please select the role of the employee",
+            choices: ["Manager", "Engineer", "Intern"],
+        }
+    ];
 
-const writeFile = fileContent => {
-    fs.writeFile('./dist/index.html', fileContent, err => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      else{
-        console.log( 'Your page has been created!');
-      }
-    });
-  };
-
-  console.log(`Welcome to the Team Profile Generator Page!`);
-
-promptStaff().then(data => generatePage(data))
-             .then(generatedHtml => writePage(generatedHtml))
-             .catch(err => console.log(err));
+    // Add more staff
+    function addNewStaff() {
+        inquirer.prompt(selectStaffRole)
+            .then(answer => {
+                if (answer.staffRole === "Manager") {
+                    if (canAddManager) {
+                        inquirer.prompt(questions.Manager)
+                            .then(answer => {
+                                //save person's info
+                                const manager = new Manager
+                                    (
+                                        answer.name,
+                                        answer.idNumber,
+                                        answer.email,
+                                        answer.officeNumber
+                                    );
+                                team.push(manager);
+                                addManager = false;
+                                if (answer.addNew === "yes") {
+                                    addNewStaff();
+                                } else {
+                                    generate();
+                                }
+                            });
+                    } else {
+                        console.log("There is a manager already!")
+                        addNewStaff();
+                    }
+    
+    
+                } else if (answer.staffRole === "Engineer") {
+                    inquirer.prompt(questions.Engineer)
+                        .then(answer => {
+                            //save info
+                            const engineer = new Engineer
+                                (
+                                    answer.name,
+                                    answer.idNumber,
+                                    answer.email,
+                                    answer.github
+                                );
+                            team.push(engineer);
+                            if (answer.addNew === "yes") {
+                                addNewStaff();
+                            } else {
+                                generate();
+                            };
+                        });
+    
+                } else if (answer.staffRole === "Intern") {
+                    inquirer.prompt(questions.Intern)
+                        .then(answer => {
+                            //save info
+                            const intern = new Intern
+                                (
+                                    answer.name,
+                                    answer.idNumber,
+                                    answer.email,
+                                    answer.school
+                                );
+                            team.push(intern);
+                            if (answer.addNew === "yes") {
+                                addNewStaff();
+                            } else {
+                                generate();
+                            };
+                        });
+                };
+            });
+    };
+    
+    addNewStaff();
+    
+    function generate() {
+        fs.writeFileSync(outputPath, render(team), "utf-8");
+        process.exit(0);
+    }
--- src/generateMarkdown.js
+++ src/generateMarkdown.js
@@ -5,23 +5,4 @@ const Intern = require("./lib/intern.js");
 
 const addAdditional = staffInfo => {
 
-  return `
-    <!DOCTYPE html> 
-    <html lang="en"> 
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Portfolio Demo</title>
-    </head>
-    ${generatePage(name)}
- 
-    <body>
-      <h1></h1>
-    </body>
-    </html>
-    `;
-  };
 
-
-module.exports = generatePage;
