e88b2deab2f94c83e0595fa46fb10c5e254bba22
Amna Syeda <amna.syeda98@gmail.com>
2021-06-27 16:10:48 -0400

    syntax error for index.js

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const fs = require("fs");
 const Manager = require("./lib/manager.js");
 const Engineer = require("./lib/engineer.js");
 const Intern = require("./lib/intern.js");
-const generateMarkdown = require('./src/generateMarkdown.js');
-const renderHTML = require("./lib/generateHTML");
-const allEmployees = [];
 
-let employeesArr = [];
+const renderHTML = require("./lib/generatehtml.js");
+const RESULTS_DIR = path.resolve(__dirname, "results");
+const resultsPath = path.join(RESULTS_DIR, "team.html");
+const team = [];
+
+function startApp() {
+    return inquirer.prompt(questions);
+};
 
 const questions = [
     {
@@ -27,8 +31,8 @@ const questions = [
         type: "input",
         name: "idNumber",
         message: "What is the ID number of the employee? (Required)",
-        validate: StaffID => {
-            if (StaffID) {
+        validate: IDNumber => {
+            if (IDNumber) {
                 return true;
             } else {
                 console.log("Please enter the employee's ID number!");
@@ -63,7 +67,7 @@ const questions = [
             }
         }
     }
-    ]
+    ],
 
     // Manager question
     managerQuestions = [
@@ -77,9 +81,8 @@ const questions = [
                 } else {
                   console.log("Please enter an office number!");
                   return false;
-                },
+                }
               },
-              {
                 type: "list",
                 name: "addNew",
                 message: "Do you want to add another employee",
@@ -99,9 +102,8 @@ const questions = [
                 } else {
                   console.log("Please enter a GitHub username!");
                   return false;
-                },
+                }
             },
-            {
               type: "list",
               name: "addNew",
               message: "Do you want to add another employee",
@@ -121,100 +123,113 @@ const questions = [
                 } else {
                   console.log("Please enter a valid school name!");
                   return false;
-                },
+                }
             },
-            {
               type: "list",
               name: "addNew",
               message: "Do you want to add another employee",
               choices: ["yes", "no"]
           }
-       ]
+       ],
 
-    // Employee's role
-    const seletRole = [
-        {
-            type: "list",
-            name: "selectRole",
-            message: "Please select the role of the employee",
-            choices: ["Manager", "Engineer", "Intern"],
-        }
-    ];
-
-    // Add more staff
-    function addNewStaff() {
+     // Add more staff
+     const promptNewStaff = ()=>{
+        console.log(`
+      =================
+      Add a New Employee
+      =================
+      `);
         inquirer.prompt(selectStaffRole)
+                .then(answer => {
+            if (answer.staffRole === "Manager") {
+            if (canAddManager) {
+            inquirer.prompt(questions.Manager)
             .then(answer => {
-                if (answer.staffRole === "Manager") {
-                    if (canAddManager) {
-                        inquirer.prompt(questions.Manager)
-                            .then(answer => {
-                                //save person's info
-                                const manager = new Manager
-                                    (
-                                        answer.name,
-                                        answer.idNumber,
-                                        answer.email,
-                                        answer.officeNumber
-                                    );
-                                team.push(manager);
-                                addManager = false;
-                                if (answer.addNew === "yes") {
-                                    addNewStaff();
-                                } else {
-                                    generate();
-                                }
-                            });
-                    } else {
-                        console.log("There is a manager already!")
-                        addNewStaff();
-                    }
+        //save person's info
+        const manager = new Manager
+           (
+            answer.name,
+            answer.idNumber,
+            answer.email,
+            answer.officeNumber
+           );
+        team.push(manager);
+        addManager = false;
+        if (answer.addNew === "yes") {
+            promptNewStaff();
+        } else {
+            generate();
+        }
+    });
+        } else {
+    console.log("There is a manager already!")
+            promptNewStaff();
+        }
+        
+        
+        } else if (answer.staffRole === "Engineer") {
+            inquirer.prompt(questions.Engineer)
+                    .then(answer => {
+        //save info
+        const engineer = new Engineer
+            (
+            answer.name,
+            answer.idNumber,
+            answer.email,
+            answer.github
+            );
+        team.push(engineer);
+        if (answer.addNew === "yes") {
+            promptNewStaff();
+        } else {
+            generate();
+        };
+    });
+        
+        } else if (answer.staffRole === "Intern") {
+            inquirer.prompt(questions.Intern)
+                    .then(answer => {
+        //save info
+        const intern = new Intern
+            (
+            answer.name,
+            answer.idNumber,
+            answer.email,
+            answer.school
+            );
+        team.push(intern);
+        if (answer.addNew === "yes") {
+            promptNewStaff();
+        } else {
+            generate();
+                     };
+                 });
+             };
+        });
+     };
     
-    
-                } else if (answer.staffRole === "Engineer") {
-                    inquirer.prompt(questions.Engineer)
-                        .then(answer => {
-                            //save info
-                            const engineer = new Engineer
-                                (
-                                    answer.name,
-                                    answer.idNumber,
-                                    answer.email,
-                                    answer.github
-                                );
-                            team.push(engineer);
-                            if (answer.addNew === "yes") {
-                                addNewStaff();
-                            } else {
-                                generate();
-                            };
-                        });
-    
-                } else if (answer.staffRole === "Intern") {
-                    inquirer.prompt(questions.Intern)
-                        .then(answer => {
-                            //save info
-                            const intern = new Intern
-                                (
-                                    answer.name,
-                                    answer.idNumber,
-                                    answer.email,
-                                    answer.school
-                                );
-                            team.push(intern);
-                            if (answer.addNew === "yes") {
-                                addNewStaff();
-                            } else {
-                                generate();
-                            };
-                        });
-                };
-            });
+const writeFile = fileContent => {
+      fs.writeFile('./results/team.html', fileContent, err => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+  
+        else{
+
+          console.log("Your page has been created!");
+        }
+      });
     };
-    
-    addNewStaff();
-    
-    function generate() {
-        fs.writeFileSync(outputPath, render(team), "utf-8");
-        process.exit(0);
-    }
+
+
+
+//const questions = 
+promptNewStaff().then(team =>{
+    return generatePage(team);
+}).then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .catch(err => {
+    console.log(err);
+  });
--- src/generateMarkdown.js
+++
@@ -1,8 +0,0 @@
-const Manager = require("./lib/manager.js");
-const Employee = require("./lib/employee.js");
-const Engineer = require("./lib/engineer.js");
-const Intern = require("./lib/intern.js");
-
-const addAdditional = staffInfo => {
-
-
