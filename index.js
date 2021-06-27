d61fbc1471112ff941320aa1edc88e09a0a8470e
Amna Syeda <amna.syeda98@gmail.com>
2021-06-26 21:36:15 -0400

    index.js user input

--- index.js
+++ index.js
@@ -4,10 +4,117 @@ const Manager = require("./lib/manager.js");
 const Employee = require("./lib/employee.js");
 const Engineer = require("./lib/engineer.js");
 const Intern = require("./lib/intern.js");
+const generateMarkdown = require('./src/generateMarkdown.js');
 const allEmployees = [];
 
 const questions = [
     {
-        
+        type: 'list',
+        name: 'role',
+        message: 'What is the position/role of the employee?',
+        choices: ['Manager', 'Employee','Engineer', 'Intern']
+    },
+    {
+        type: 'input',
+        name: 'firstName',
+        message: ({ role }) => `What is the indivisual's first name?`,
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter a name!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'lastName',
+        message: ({ firstName }) => `What is the indivisual's last name?`,
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter a name!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: ({ firstName }) => `What is the indivisual's ID number?`,
+        validate: idInput => {
+            if (!isNaN(parseInt(idInput))) {
+                return true;
+            } else {
+                console.log('Please enter a valid ID number!');
+                return false;
+            }
+        }
+    },
+
+    {
+        type: 'input',
+        name: 'github',
+        message: ({ firstName }) => `What is the indivisual's GitHub userame?`,
+        when: ({ role }) => {
+            if (role === 'engineer') {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please enter a username!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: ({ firstName }) => `What school does the indivisual's go to?`,
+        when: ({ role }) => {
+            if (role === 'Intern') {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        validate: schoolInput => {
+            if (schoolInput) {
+                return true;
+            } else {
+                console.log('Please enter a valid school name!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'addAdditional',
+        message: 'Would you like to add an additional employee?',
+        default: true
     }
 ]
+
+const promptUser = () => {
+
+    return inquirer.prompt(questions)
+    .then(userResponse => {
+
+     
+        allEmployees.push(userResponse);
+
+
+        if (userResponse.addEmployee) {
+            return promptUser();
+        } else {
+            return allEmployees;
+        };
+    });
+};
