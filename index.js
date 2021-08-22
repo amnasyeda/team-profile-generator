d657d98cc69cdb12e7e923e1a083f29bc7fc0ab4
Amna Syeda <amna.syeda98@gmail.com>
2021-08-22 12:09:37 -0400

    fixed error for employee selection

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const inquirer = require("inquirer");
-const fs = require("fs");
+const inquirer = require('inquirer');
+const fs = require('fs');
 const { log } = require("console");
 const path = require("path");
 
